7322c617dd809626f277e0e9a970736a77062572
agenteaves <newmodshere@gmail.com>
2026-08-04 14:09:08 -0500

    Update forge-scanner.js

--- meme-forge/upgrades/forge-scanner.js
+++ meme-forge/upgrades/forge-scanner.js
@@ -1,1 +1,70 @@
+////////////////////////////////////////////////////
+// SPARKD FORGE SCANNER v0.1
+// Reads PNG Forge DNA
+////////////////////////////////////////////////////
 
+
+window.SPARKD_SCANNER = {
+
+
+scan:function(file){
+
+
+    const reader = new FileReader();
+
+
+    reader.onload = function(e){
+
+
+        const bytes =
+        new Uint8Array(e.target.result);
+
+
+        const text =
+        new TextDecoder()
+        .decode(bytes);
+
+
+
+        if(text.includes("SPARKD-FORGE")){
+
+
+            console.log(
+                "🔥 SPARKD FORGE DETECTED"
+            );
+
+
+            const start =
+            text.indexOf("SPARKD-FORGE");
+
+
+            console.log(
+                text.substring(
+                    start,
+                    start + 500
+                )
+            );
+
+
+        }
+        else{
+
+
+            console.log(
+                "❌ No SPARKD Forge DNA found"
+            );
+
+
+        }
+
+
+    };
+
+
+    reader.readAsArrayBuffer(file);
+
+
+}
+
+
+};
