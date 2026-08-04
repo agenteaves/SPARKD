d5a5604c2a626250a13834baa8b77b32b1422cbe
agenteaves <newmodshere@gmail.com>
2026-08-04 13:28:36 -0500

    Update png-forge-writer.js

--- meme-forge/upgrades/png-forge-writer.js
+++ meme-forge/upgrades/png-forge-writer.js
@@ -1,1 +1,76 @@
+////////////////////////////////////////////////////
+// SPARKD PNG FORGE WRITER v0.1
+// Metadata Container Prototype
+////////////////////////////////////////////////////
 
+
+window.SPARKD_PNG = {
+
+
+    attach:function(canvas, forgeRecord){
+
+
+        console.log(
+            "🔥 SPARKD PNG Forge preparing file"
+        );
+
+
+        const payload = {
+
+
+            forge:
+            "SPARKD Meme Forge",
+
+
+            version:
+            forgeRecord.version,
+
+
+            memeID:
+            forgeRecord.memeID,
+
+
+            DNA:
+            forgeRecord.DNA,
+
+
+            imageFingerprint:
+            forgeRecord.imageFingerprint,
+
+
+            created:
+            forgeRecord.created,
+
+
+            contract:
+            forgeRecord.contract
+
+
+        };
+
+
+        console.log(
+            "🔥 PNG Metadata Payload:",
+            payload
+        );
+
+
+        /*
+            Temporary version:
+
+            Returns normal PNG.
+
+            Next version will inject
+            this payload into PNG chunks.
+        */
+
+
+        return canvas.toDataURL(
+            "image/png"
+        );
+
+
+    }
+
+
+};
