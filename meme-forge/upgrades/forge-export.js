971122a80247fe4c89b490863e1f655c171eb1fd
agenteaves <newmodshere@gmail.com>
2026-08-04 12:28:22 -0500

    Update forge-export.js

--- meme-forge/upgrades/forge-export.js
+++ meme-forge/upgrades/forge-export.js
@@ -1,1 +1,36 @@
+////////////////////////////////////////////////////
+// SPARKD FORGE EXPORT v0.1
+// Hidden PNG Origin Data
+////////////////////////////////////////////////////
 
+
+window.SPARKD_EXPORT = {
+
+
+    attachForgeData:function(canvas, forgeRecord){
+
+
+        const data = {
+
+            SPARKD:
+            "Meme Forge",
+
+            Forge:
+            forgeRecord
+
+        };
+
+
+        console.log(
+            "🔥 Hidden Forge Layer:",
+            data
+        );
+
+
+        return data;
+
+
+    }
+
+
+};
