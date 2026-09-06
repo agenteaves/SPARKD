71bbfa5a071aa74065faee71964699039bd21389
agenteaves <newmodshere@gmail.com>
2026-09-06 10:14:48 -0500

    Add SPARKD Man AI config

---
+++ sparkd-man-ai/sparkd-man-config.js
@@ -0,0 +1,9 @@
+(() => {
+  "use strict";
+  window.SPARKD_MAN_CONFIG = Object.freeze({
+    endpoint: "https://uxpbgzksfizkyxubctep.supabase.co/functions/v1/sparkd-man-ai",
+    wakePhrase: "hey spark",
+    followUpWindowMs: 15000,
+    maxHistoryMessages: 6
+  });
+})();
