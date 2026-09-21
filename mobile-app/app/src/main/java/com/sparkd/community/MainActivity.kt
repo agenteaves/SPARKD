4dfcb234f9d08f81a059231acb64a5a7f5c57865
agenteaves <newmodshere@gmail.com>
2026-09-21 07:46:40 -0500

    Launch production mobile experience

--- mobile-app/app/src/main/java/com/sparkd/community/MainActivity.kt
+++ mobile-app/app/src/main/java/com/sparkd/community/MainActivity.kt
@@ -2,4 +2,10 @@ package com.sparkd.community
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-class MainActivity:ComponentActivity(){override fun onCreate(savedInstanceState:Bundle?){super.onCreate(savedInstanceState);setContent{SparkdApp()}}}
+class MainActivity:ComponentActivity(){
+ private lateinit var wallet:WalletSession
+ override fun onCreate(savedInstanceState:Bundle?){super.onCreate(savedInstanceState)
+  wallet=WalletSession(this,lifecycle).also { session -> session.register { contract, callback -> registerForActivityResult(contract, callback) } }
+  setContent{ProductionApp(wallet)}
+ }
+}
