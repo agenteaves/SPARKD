b46daedeb345080f0e3f2e9d84942aa9bb8d9f21
agenteaves <newmodshere@gmail.com>
2026-09-21 07:54:49 -0500

    Fix production mobile build opt-in

--- mobile-app/app/src/main/java/com/sparkd/community/ProductionApp.kt
+++ mobile-app/app/src/main/java/com/sparkd/community/ProductionApp.kt
@@ -10,6 +10,7 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import kotlinx.coroutines.launch
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable fun ProductionApp(wallet: WalletSession) {
     val repo = remember { LiveRepository() }
     var page by remember { mutableStateOf("home") }
