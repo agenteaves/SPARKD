0dde5d423793cfe89a17db43552e64b8de0f5c12
agenteaves <newmodshere@gmail.com>
2026-09-22 04:26:49 -0500

    Release Android 1.0.3 build 6 updater fix

--- mobile-app/app/build.gradle.kts
+++ mobile-app/app/build.gradle.kts
@@ -1,7 +1,7 @@
 plugins { id("com.android.application"); id("org.jetbrains.kotlin.android"); id("org.jetbrains.kotlin.plugin.compose") }
 android {
  namespace="com.sparkd.community"; compileSdk=35
- defaultConfig { applicationId="com.sparkd.community"; minSdk=26; targetSdk=35; versionCode=5; versionName="1.0.2" }
+ defaultConfig { applicationId="com.sparkd.community"; minSdk=26; targetSdk=35; versionCode=6; versionName="1.0.3" }
  compileOptions { sourceCompatibility=JavaVersion.VERSION_17; targetCompatibility=JavaVersion.VERSION_17 }
  kotlinOptions { jvmTarget="17" }
  buildFeatures { compose=true }
