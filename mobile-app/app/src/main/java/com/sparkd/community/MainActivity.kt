8155b7062f47d2b34418201026a98f45ad0a95f5
agenteaves <newmodshere@gmail.com>
2026-09-21 22:27:26 -0500

    Register Jupiter-only wallet activity flow

--- mobile-app/app/src/main/java/com/sparkd/community/MainActivity.kt
+++ mobile-app/app/src/main/java/com/sparkd/community/MainActivity.kt
@@ -1,16 +1,11 @@
 package com.sparkd.community
-
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import com.solana.mobilewalletadapter.clientlib.ActivityResultSender
-
-class MainActivity : ComponentActivity() {
-    private lateinit var wallet: WalletSession
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        wallet = WalletSession(ActivityResultSender(this))
-        setContent { ProductionApp(wallet) }
-    }
+class MainActivity:ComponentActivity(){
+ private lateinit var wallet:WalletSession
+ override fun onCreate(savedInstanceState:Bundle?){super.onCreate(savedInstanceState)
+  wallet=WalletSession(this,lifecycle).also { session -> session.register { contract, callback -> registerForActivityResult(contract, callback) } }
+  setContent{ProductionApp(wallet)}
+ }
 }
