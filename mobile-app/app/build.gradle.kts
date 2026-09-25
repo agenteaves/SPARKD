8f4d8220c516a370cedf6c33f65a4b9b24c1a39c
agenteaves <newmodshere@gmail.com>
2026-09-25 18:49:07 -0500

    Release Android 1.0.25 build 28

--- mobile-app/app/build.gradle.kts
+++ mobile-app/app/build.gradle.kts
@@ -1,7 +1,7 @@
 plugins { id("com.android.application"); id("org.jetbrains.kotlin.android"); id("org.jetbrains.kotlin.plugin.compose") }
 android {
  namespace="com.sparkd.community"; compileSdk=35
- defaultConfig { applicationId="com.sparkd.community"; minSdk=26; targetSdk=35; versionCode=27; versionName="1.0.24" }
+ defaultConfig { applicationId="com.sparkd.community"; minSdk=26; targetSdk=35; versionCode=28; versionName="1.0.25" }
  compileOptions { sourceCompatibility=JavaVersion.VERSION_17; targetCompatibility=JavaVersion.VERSION_17 }
  kotlinOptions { jvmTarget="17" }
  buildFeatures { compose=true }
